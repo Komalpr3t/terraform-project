a20c9ef0d68df5794732944a10fa064e08837335
Komalpr3t <kgrewal1703@gmail.com>
2026-01-12 10:36:15 +0530

    Initial Terraform project with dev and prod tfvars

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = "terraform-${var.env}"
+    Environment = var.env
+  }
+}
